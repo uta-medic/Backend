fa549990e553d6fddc184193a6ded44912a73d1a
GaboBv <mamanifarfangabriel@gmail.com>
2026-07-20 14:00:42 -0400

    check 3.0

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { createDatabaseConfig } from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): TypeOrmModuleOptions =>
+        createDatabaseConfig(configService),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
