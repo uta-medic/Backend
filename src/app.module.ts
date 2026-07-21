dd7c0ba797c9e7190337316393022c5f833fb7e5
JHAMILCALI <jhamilquea@gmail.com>
2026-07-21 11:43:16 -0400

    feat: agregar importación de 'mssql' en el módulo de la aplicación

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import 'mssql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
